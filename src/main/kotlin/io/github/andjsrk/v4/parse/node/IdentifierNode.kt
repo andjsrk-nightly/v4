18c95ec8b1701c287d5cfb8321619e28afa757eb
andjsrk <andjsrk0213.nightly@gmail.com>
2023-04-23 10:57:18 +0900

    IdentifierNode open 해제

--- src/main/kotlin/io/github/andjsrk/v4/parse/node/IdentifierNode.kt
+++ src/main/kotlin/io/github/andjsrk/v4/parse/node/IdentifierNode.kt
@@ -3,7 +3,7 @@ package io.github.andjsrk.v4.parse.node
 import io.github.andjsrk.v4.parse.stringifyLikeDataClass
 import io.github.andjsrk.v4.tokenize.Token
 
-open class IdentifierNode(token: Token): ExpressionNode {
+class IdentifierNode(token: Token): ExpressionNode {
     val value = token.rawContent
     override val range = token.range
     override fun toString() =

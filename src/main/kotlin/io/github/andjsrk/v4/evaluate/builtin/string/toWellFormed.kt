5f401be1dd4e7119a9d17c6d0cf24397c0a98afb
andjsrk <andjsrk0213.nightly@gmail.com>
2023-07-16 11:15:17 +0900

    const 왜빼먹었지

--- src/main/kotlin/io/github/andjsrk/v4/evaluate/builtin/string/toWellFormed.kt
+++ src/main/kotlin/io/github/andjsrk/v4/evaluate/builtin/string/toWellFormed.kt
@@ -5,7 +5,7 @@ import io.github.andjsrk.v4.evaluate.requireToBeString
 import io.github.andjsrk.v4.evaluate.type.Completion
 import io.github.andjsrk.v4.evaluate.type.lang.builtinMethod
 
-private val REPLACEMENT_CHARACTER = '\uFFFD'
+private const val REPLACEMENT_CHARACTER = '\uFFFD'
 
 val toWellFormed = builtinMethod("toWellFormed") fn@ { thisArg, args ->
     val string = thisArg.requireToBeString { return@fn it }

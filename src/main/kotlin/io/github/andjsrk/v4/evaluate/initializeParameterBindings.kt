2ea668a216692a78eeb87d5a1a14d845981d69c9
andjsrk <andjsrk0213.nightly@gmail.com>
2023-09-15 06:48:16 +0900

    업데이트

--- src/main/kotlin/io/github/andjsrk/v4/evaluate/initializeParameterBindings.kt
+++ src/main/kotlin/io/github/andjsrk/v4/evaluate/initializeParameterBindings.kt
@@ -1,10 +1,9 @@
 package io.github.andjsrk.v4.evaluate
 
-import io.github.andjsrk.v4.CompilerFalsePositive
+import io.github.andjsrk.v4.*
 import io.github.andjsrk.v4.error.TypeErrorKind
 import io.github.andjsrk.v4.evaluate.type.*
 import io.github.andjsrk.v4.evaluate.type.lang.*
-import io.github.andjsrk.v4.neverHappens
 import io.github.andjsrk.v4.parse.isAnonymous
 import io.github.andjsrk.v4.parse.node.*
 import io.github.andjsrk.v4.parse.stringValue
@@ -21,9 +20,25 @@ private fun List<MaybeRestNode>.initializeParameterBindings(valuesIterator: Iter
                         val value = valuesIterator.next()
                             .orReturn { return it }
                         ref.putOrInitializeBinding(value, env)
+                            .orReturn { return it }
                     }
-                    is ArrayBindingPatternNode -> TODO()
-                    is ObjectBindingPatternNode -> TODO()
+                    is ArrayBindingPatternNode -> {
+                        val arr = valuesIterator.next()
+                            .orReturn { return it }
+                        require(arr is ArrayType)
+                        val iter = arr.array
+                            .mapAsSequence { it.toNormal() }
+                            .iterator()
+                            .toRestCollectedArrayIterator(binding.elements)
+                        binding.elements.initializeParameterBindings(iter, env)
+                    }
+                    is ObjectBindingPatternNode -> {
+                        val arr = valuesIterator.next()
+                            .orReturn { return it }
+                        val iter = arr.toRestCollectedObjectIterator(binding.elements)
+                        binding.elements.initializeParameterBindings(iter, env)
+                            .orReturn { return it }
+                    }
                     else ->
                         @CompilerFalsePositive
                         neverHappens()
@@ -33,70 +48,26 @@ private fun List<MaybeRestNode>.initializeParameterBindings(valuesIterator: Iter
                 when (val binding = element.binding) {
                     is IdentifierNode -> {
                         val ref = resolveBinding(binding.stringValue, env)
-                        val value = element.getArgValueOrDefault(valuesIterator, size, i, binding.stringValue)
+                        val value = element.getValueOrDefault(valuesIterator, size, i, binding.stringValue)
                             .orReturn { return it }
                         ref.putOrInitializeBinding(value, env)
                             .orReturn { return it }
                     }
                     is ArrayBindingPatternNode -> {
-                        val value = element.getArgValueOrDefault(valuesIterator, size, i)
+                        val value = element.getValueOrDefault(valuesIterator, size, i)
                             .orReturn { return it }
                         val valueIter = iterableToSequence(value)
                             .orReturn { return it }
                             .value
                             .iterator()
-                        val iter = iterator {
-                            for (elem in binding.elements) {
-                                when (elem) {
-                                    is NonRestNode ->
-                                        if (valueIter.hasNext()) yield(valueIter.next())
-                                        else return@iterator
-                                    is RestNode -> {
-                                        val values = valueIter.toLanguageValueList()
-                                            .orReturn {
-                                                yield(it)
-                                                return@iterator
-                                            }
-                                        yield(
-                                            ImmutableArrayType(values)
-                                                .toNormal()
-                                        )
-                                    }
-                                }
-                            }
-                        }
+                        val iter = valueIter.toRestCollectedArrayIterator(binding.elements)
                         binding.elements.initializeParameterBindings(iter, env)
                             .orReturn { return it }
                     }
                     is ObjectBindingPatternNode -> {
-                        val value = element.getArgValueOrDefault(valuesIterator, size, i)
+                        val value = element.getValueOrDefault(valuesIterator, size, i)
                             .orReturn { return it }
-                        val keys = mutableListOf<PropertyKey>()
-                        val iter = binding.elements
-                            .asSequence()
-                            .map {
-                                when (it) {
-                                    is RestNode -> {
-                                        val maybeObj = value as? ObjectType
-                                        val rest = maybeObj?.run {
-                                            val props = properties.toMutableMap() // clone the original object first
-                                            // syntactically rest element must be the last,
-                                            // so we can sure that appropriate keys are added to the list `key`
-                                            for (key in keys) props.remove(key)
-                                            ObjectType.createNormal(props)
-                                        } ?: ObjectType.createNormal() // since primitive values cannot have any own properties, we can sure that the result is an empty object
-                                        rest.toNormal() // an object that contains the other own enumerable properties of the value
-                                    }
-                                    is NonRestObjectPropertyNode -> {
-                                        val key = it.key.toPropertyKey()
-                                            .orReturn { return@map it }
-                                        keys += key
-                                        value.getProperty(key)
-                                    }
-                                    is NonRestNode -> neverHappens()
-                                }
-                            }
-                            .iterator()
+                        val iter = value.toRestCollectedObjectIterator(binding.elements)
                         binding.elements.initializeParameterBindings(iter, env)
                             .orReturn { return it }
                     }
@@ -109,15 +80,15 @@ private fun List<MaybeRestNode>.initializeParameterBindings(valuesIterator: Iter
     }
     return empty
 }
-private fun NonRestNode.getArgValueOrDefault(
-    argsIterator: Iterator<NonEmptyNormalOrAbrupt>,
+private fun NonRestNode.getValueOrDefault(
+    valuesIterator: Iterator<NonEmptyNormalOrAbrupt>,
     expectedCount: Int,
     index: Int,
     paramName: StringType? = null,
 ): NonEmptyNormalOrAbrupt {
     val value = when {
-        argsIterator.hasNext() ->
-            argsIterator.next()
+        valuesIterator.hasNext() ->
+            valuesIterator.next()
                 .orReturn { return it }
         default == null -> return throwError(TypeErrorKind.ITERABLE_YIELDED_INSUFFICIENT_NUMBER_OF_VALUES, expectedCount.toString(), index.toString())
         else -> NullType
@@ -130,4 +101,67 @@ private fun NonRestNode.getArgValueOrDefault(
         } else value
     )
         .toNormal()
+}
+/**
+ * Transforms the iterator to simple form to initialize a binding easily.
+ *
+ * - non-rest element: collect the element as it is
+ * - rest element: collect the remaining elements as an array
+ */
+private fun Iterator<NonEmptyNormalOrAbrupt>.toRestCollectedArrayIterator(bindingElements: List<MaybeRestNode>) =
+    iterator {
+        for (elem in bindingElements) {
+            when (elem) {
+                is NonRestNode ->
+                    if (hasNext()) yield(next())
+                    else return@iterator
+                is RestNode -> {
+                    val values = toLanguageValueList()
+                        .orReturn {
+                            yield(it)
+                            return@iterator
+                        }
+                    yield(
+                        ImmutableArrayType(values)
+                            .toNormal()
+                    )
+                }
+            }
+        }
+    }
+/**
+ * Transforms the value to an iterator with simple form to initialize a binding easily.
+ *
+ * - non-rest element: collect the value of the property
+ * - rest element: collect the remaining own enumerable properties as an object
+ */
+private fun LanguageType.toRestCollectedObjectIterator(bindingElements: List<MaybeRestNode>): Iterator<NonEmptyNormalOrAbrupt> {
+    val nonRestKeys = mutableSetOf<PropertyKey>()
+    return bindingElements
+        .asSequence()
+        .map { elem ->
+            when (elem) {
+                is RestNode -> {
+                    val maybeObj = this as? ObjectType
+                    val rest = maybeObj?.run {
+                        val props = properties.toMutableMap() // clone the original object first
+                        // syntactically rest element must be the last,
+                        // so we can sure that appropriate keys are added to the list `key`
+                        props -= nonRestKeys
+                        props.entries.removeAll { (_, prop) -> prop.not { enumerable } }
+                        ObjectType.createNormal(props)
+                    }
+                        ?: ObjectType.createNormal() // since primitive values cannot have any own properties, we can sure that the result is an empty object
+                    rest.toNormal() // an object that contains the other own enumerable properties of the value
+                }
+                is NonRestObjectPropertyNode -> {
+                    val key = elem.key.toPropertyKey()
+                        .orReturn { return@map it }
+                    nonRestKeys += key
+                    getProperty(key)
+                }
+                is NonRestNode -> neverHappens()
+            }
+        }
+        .iterator()
 }
--- src/main/kotlin/io/github/andjsrk/v4/evaluate/toLanguageValueList.kt
+++ src/main/kotlin/io/github/andjsrk/v4/evaluate/toLanguageValueList.kt
@@ -5,30 +5,32 @@ import io.github.andjsrk.v4.evaluate.type.lang.LanguageType
 import io.github.andjsrk.v4.parse.node.*
 
 fun Iterator<NonEmptyNormalOrAbrupt>.toLanguageValueList(includeReturnValue: Boolean = false): MaybeAbrupt<ListType<LanguageType>> {
-    val values = mutableListOf<LanguageType>()
-    while (hasNext()) {
-        val value = next()
-            .orReturn { return it }
-        if (hasNext() || includeReturnValue) values += value
-    }
+    val iter =
+        if (includeReturnValue) this
+        else withoutLast()
+    val values = iter
+        .asSequence()
+        .toList()
+        .map {
+            it.orReturn { return it }
+        }
     return ListType(values)
         .toWideNormal()
 }
 
 fun List<MaybeSpreadNode>.toLanguageValueList(): MaybeAbrupt<ListType<LanguageType>> {
-    val values = mutableListOf<LanguageType>()
-    for (elem in this) {
+    val values = flatMap { elem ->
         val value = elem.expression.evaluateValue()
             .orReturn { return it }
         when (elem) {
-            is NonSpreadNode -> values += value
+            is NonSpreadNode -> listOf(value)
             is SpreadNode ->
                 iterableToSequence(value)
                     .orReturn { return it }
                     .value
-                    .forEachYielded {
-                        values += it.orReturn { return it }
-                    }
+                    .iterator()
+                    .toLanguageValueList()
+                    .orReturn { return it }
         }
     }
     return ListType(values)
